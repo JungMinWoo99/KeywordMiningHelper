e9b681d2b2d89cd58ce655f41c066609b530b5f2
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-11-22 12:59:45 +0900

    UI 배치 수정 및 버튼 추가
    - 전체적인 UI 배치 수정
    - 하위 URL 추출, URL 텍스트 파일 로드 버튼 추가
    - TODO : 새로 추가한 버튼에 동작할 함수 구현

--- client_sample.py
+++ client_sample.py
@@ -9,7 +9,7 @@ import matplotlib.colors as mcolors
 
 window = tkinter.Tk()
 window.title("Keyword Mining Helper")
-window.geometry("1000x900")
+window.geometry("900x660")
 window.resizable(False, False)
 
 # 키워드 추출
@@ -29,6 +29,13 @@ def add_url():
         url_list_box.insert(tkinter.END, url)
         url_input.delete("1.0", "end")
 
+# 구현 필요...
+def extract_sub_url():
+    pass
+
+# 구현 필요...
+def load_url_text_file():
+    pass
 
 def remove_url():
     current_select_url = url_list_box.curselection()
@@ -102,99 +109,99 @@ def wc_gen():
                                           background_color=background_color)
     wc_generator.create_wordcloud()
 
-
 # url 입력
-
 url_label = tkinter.Label(window, text="URL", width=5, height=1)
-url_label.place(x=10, y=10)
+url_label.place(x=10, y=13)
 
 url_input = tkinter.Text(window, width=80, height=1)
-url_input.place(x=50, y=10)
+url_input.place(x=50, y=14)
 
 url_add_button = tkinter.Button(window, text="추가하기", width=15, height=1, command=add_url, repeatdelay=1000,
                                 repeatinterval=100)
 url_add_button.place(x=620, y=10)
 
 # url 목록 출력
-
 url_list_label = tkinter.Label(window, text="URL 목록", width=10, height=1)
-url_list_label.place(x=10, y=50)
+url_list_label.place(x=1, y=50)
 
 url_list_box = tkinter.Listbox(window, width=80, height=20)
 url_list_box.place(x=10, y=70)
 
-remove_url_button = tkinter.Button(window, text="삭제하기", width=15, height=1, command=remove_url, repeatdelay=1000,
-                                   repeatinterval=100)
-remove_url_button.place(x=620, y=100)
+extract_sub_url_button = tkinter.Button(window, text="하위 URL 추출", width=20, height=1, command=extract_sub_url, repeatdelay=1000, repeatinterval=100)
+extract_sub_url_button.place(x=600, y=80)
 
-clear_url_button = tkinter.Button(window, text="초기화", width=15, height=1, command=clear_url, repeatdelay=1000,
-                                  repeatinterval=100)
-clear_url_button.place(x=620, y=140)
+load_url_text_file_button = tkinter.Button(window, text="URL 텍스트 파일 로드", width=20, height=1, command=load_url_text_file, repeatdelay=1000, repeatinterval=100)
+load_url_text_file_button.place(x=600, y=120)
 
-mining_keyword_button = tkinter.Button(window, text="키워드 추출", width=15, height=1, command=mining_keyword,
-                                       repeatdelay=1000, repeatinterval=100)
-mining_keyword_button.place(x=620, y=180)
+remove_url_button = tkinter.Button(window, text="삭제하기", width=20, height=1, command=remove_url, repeatdelay=1000, repeatinterval=100)
+remove_url_button.place(x=600, y=160)
+
+clear_url_button = tkinter.Button(window, text="초기화", width=20, height=1, command=clear_url, repeatdelay=1000, repeatinterval=100)
+clear_url_button.place(x=600, y=200)
+
+mining_keyword_button = tkinter.Button(window, text="키워드 추출", width=20, height=1, command=mining_keyword, repeatdelay=1000, repeatinterval=100)
+mining_keyword_button.place(x=600, y=240)
 
 # 단어 분포 결과 출력
-
 word_distribute_label = tkinter.Label(window, text="단어 분포", width=7, height=1)
 word_distribute_label.place(x=10, y=400)
 
-word_distribute_box = tkinter.ttk.Treeview(window, height=20, columns=["one"], displaycolumns=["one"])
+word_distribute_box = tkinter.ttk.Treeview(window, height=10, columns=["one"], displaycolumns=["one"])
 word_distribute_box.place(x=10, y=420)
 
-word_distribute_box.column("#0", width=100, )
+word_distribute_box.column("#0", width=200, )
 word_distribute_box.heading("#0", text="단어")
 
-word_distribute_box.column("#1", width=100, anchor="center")
+word_distribute_box.column("#1", width=200, anchor="center")
 word_distribute_box.heading("one", text="분포수", anchor="center")
 
-keyword_del_button = tkinter.Button(window, text="단어 삭제", width=15, height=1, command=remove_word,
-                                    repeatdelay=1000, repeatinterval=100)
-keyword_del_button.place(x=620, y=400)
+keyword_del_button = tkinter.Button(window, text="단어 삭제", width=15, height=1, command=remove_word, repeatdelay=1000, repeatinterval=100)
+keyword_del_button.place(x=430, y=450)
 
-min_num_input = tkinter.Text(window, width=5, height=1)
-min_num_input.place(x=750, y=450)
+min_num_input = tkinter.Text(window, width=10, height=1)
+min_num_input.place(x=560, y=493)
 
-min_filter_button = tkinter.Button(window, text="최소빈도필터", width=15, height=1, command=filter_min,
-                                   repeatdelay=1000, repeatinterval=100)
-min_filter_button.place(x=620, y=450)
+min_filter_button = tkinter.Button(window, text="최소빈도필터", width=15, height=1, command=filter_min, repeatdelay=1000, repeatinterval=100)
+min_filter_button.place(x=430, y=490)
 
-min_filter_button = tkinter.Button(window, text="워드 클라우드 생성", width=15, height=1, command=wc_gen,
-                                   repeatdelay=1000, repeatinterval=100)
-min_filter_button.place(x=620, y=500)
+min_filter_button = tkinter.Button(window, text="워드 클라우드 생성", width=15, height=1, command=wc_gen, repeatdelay=1000, repeatinterval=100)
+min_filter_button.place(x=430, y=530)
+
+# 워드 클라우드 이미지 옵션
+image_option_label = tkinter.Label(window, text="이미지 옵션", width=10, height=1)
+image_option_label.place(x=650, y=400)
 
 background_color_label = tkinter.Label(window, text="배경색", width=5, height=1)
-background_color_label.place(x=550, y=550)
+background_color_label.place(x=650, y=430)
 background_color_combo_box = tkinter.ttk.Combobox(textvariable=str, width=20)
 background_color_combo_box['value'] = background_color_list
-background_color_combo_box.place(x=620, y=550)
+background_color_combo_box.place(x=700, y=430)
 
 colormap_label = tkinter.Label(window, text="컬러맵", width=5, height=1)
-colormap_label.place(x=550, y=600)
+colormap_label.place(x=650, y=460)
 colormap_combo_box = tkinter.ttk.Combobox(textvariable=str, width=20)
 colormap_combo_box['value'] = colormap_list
-colormap_combo_box.place(x=620, y=600)
+colormap_combo_box.place(x=700, y=460)
 
 use_image_check_var = tkinter.IntVar()
 use_image_check_button = tkinter.ttk.Checkbutton(variable=use_image_check_var)
 use_image_check_button.config(text="이미지 사용")
-use_image_check_button.place(x=620, y=650)
+use_image_check_button.place(x=650, y=490)
 
 use_colormap_check_var = tkinter.IntVar()
 use_colormap_check_button = tkinter.ttk.Checkbutton(variable=use_colormap_check_var)
 use_colormap_check_button.config(text="이미지 컬러링 사용")
-use_colormap_check_button.place(x=620, y=700)
+use_colormap_check_button.place(x=650, y=520)
 
 wc_width_label = tkinter.Label(window, text="사진 폭", width=10, height=1)
-wc_width_label.place(x=550, y=750)
-wc_width_input = tkinter.Text(window, width=5, height=1)
-wc_width_input.place(x=620, y=750)
+wc_width_label.place(x=650, y=550)
+wc_width_input = tkinter.Text(window, width=15, height=1)
+wc_width_input.place(x=750, y=550)
 
 wc_height_label = tkinter.Label(window, text="사진 높이", width=10, height=1)
-wc_height_label.place(x=550, y=800)
-wc_height_input = tkinter.Text(window, width=5, height=1)
-wc_height_input.place(x=620, y=800)
+wc_height_label.place(x=650, y=580)
+wc_height_input = tkinter.Text(window, width=15, height=1)
+wc_height_input.place(x=750, y=580)
 
 # 화면 실행
 
